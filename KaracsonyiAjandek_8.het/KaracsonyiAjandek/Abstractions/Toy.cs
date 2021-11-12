acdbf5954bc52f4644950ce550284b8edf5de1d4
Máté Mizsák <mate.mizsak@stud.uni-corvinus.hu>
2021-11-12 19:30:36 +0100

    Toy

---
+++ KaracsonyiAjandek_8.het/KaracsonyiAjandek/Abstractions/Toy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KaracsonyiAjandek.Entities
+{
+    class Toy
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        public void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+
+        }
+
+        private void DrawImage(Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
---
+++ KaracsonyiAjandek_8.het/KaracsonyiAjandek/Entities/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KaracsonyiAjandek.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint;
+        }
+
+        public void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+            
+        }
+
+        private void DrawImage(Graphics graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+
+    }
+}
