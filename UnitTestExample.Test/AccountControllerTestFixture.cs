11deef6294cd345ef9758282242892f46ee49227
Máté Mizsák <mate.mizsak@stud.uni-corvinus.hu>
2021-11-26 12:40:18 +0100

    jelszovalidalas&happypath

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -29,5 +30,33 @@ namespace UnitTestExample.Test
             Assert.AreEqual(expectedResult, actualResult);
         }
 
+        public bool ValidatePassword(string password)
+        {
+            var hasLowercase = new Regex(@"[a-z]+");
+            var hasUppercase = new Regex(@"[A-Z]+");
+            var hasNumber = new Regex(@"[0-9]+");
+            var eightlong = new Regex(@".{8,}");
+            return hasLowercase.IsMatch(password) && hasUppercase.IsMatch(password) && hasNumber.IsMatch(password) && eightlong.IsMatch(password);
+        }
+
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "Abcd1234567"),
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
     }
 }
