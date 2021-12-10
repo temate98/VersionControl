42ef59cb39409f953d3956f26eeb2ac4d9f00fdf
Máté Mizsák <mate.mizsak@stud.uni-corvinus.hu>
2021-12-10 12:27:13 +0100

    elfelejtettem commitolni

--- Evolution/Form1.cs
+++ Evolution/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Evolution
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.AddPlayer(nbrOfSteps)
+            gc.Start(true);
         }
     }
 }
